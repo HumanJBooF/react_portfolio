3ff65b1a5d67f57f3ce11faeb346bfe43675210a
HumanJBooF <humanjboof@gmail.com>
2018-12-28 00:07:16 -0500

    testing out card.group and containers from semantic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './semantic/dist/semantic.min.css';
 import Nav from './components/Nav';
-
+import Cards from './components/Card';
+import images from './images.json';
+import { Card, Container } from 'semantic-ui-react';
 class App extends React.Component {
+  state = {
+    images: images
+  }
   render () {
     return (
       <>
         <Nav />
+        <Container>
+          <Card.Group itemsPerRow={4}>
+            {this.state.images.map((image, i) =>
+              <Cards
+                image={image.image}
+                key={i} />
+            )}
+          </Card.Group>
+        </Container>
       </>
     )
   }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card } from 'semantic-ui-react';
+
+const Cards = props => (
+    <Card
+        image={props.image}
+    />
+)
+
+export default Cards;
